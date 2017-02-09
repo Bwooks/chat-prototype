4be4dfafe168e95ae43f4218aa83d31a8180d339
Bwooks <brooksdulla@gmail.com>
2017-02-09 00:03:14 -0500

    Bad merge, fixed

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "babel-polyfill";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-// render the main component
-ReactDOM.render(
-    <App/>,
-  document.getElementById('app')
-);
